4fc45f26b15b5fd9bbc4edb7020ef45465e4d40c
luke-song <hyeonminsong7@gmail.com>
2022-04-10 23:45:48 -0700

    First Commit
    Rendering 3d model on the browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stage, OrbitControls } from "@react-three/drei";
+import { Menu } from "./components/Menu";
+import { Stitch } from "./components/Stitch";
+import * as THREE from "three";
 
-function App() {
+const App = () => {
+  const orange = new THREE.Color(0xffa500);
+  const crimson = new THREE.Color(0xdc143c);
+  const teal = new THREE.Color(0x008080);
+  const steelblue = new THREE.Color(0x4682b4);
+
+  const [currentColor, setCurrentColor] = useState(orange);
+
+  const handleColorChange = (event, color) => {
+    event.preventDefault();
+    if (color === "crimson") {
+      setCurrentColor(crimson);
+    } else if (color === "teal") {
+      setCurrentColor(teal);
+    } else if (color === "steelblue") {
+      setCurrentColor(steelblue);
+    } else {
+      setCurrentColor(orange);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu handleColorChange={handleColorChange} />
+      <Canvas dpr={[1, 2]} camera={{ fov: 45 }}>
+        <color attach="background" args={["midnightBlue"]} />
+        <Suspense fallback={null}>
+          <Stage
+            environment={null}
+            intensity={1}
+            contactShadowOpacity={0.5}
+            shadowBias={-0.0015}
+          >
+            <Stitch currentColor={currentColor} />
+          </Stage>
+        </Suspense>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          enablePan={false}
+          minPolarAngle={Math.PI / 2.8}
+          maxPolarAngle={Math.PI / 2.8}
+        />
+      </Canvas>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Stitch.js
@@ -0,0 +1,19 @@
+import React, { useLayoutEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
+export const Stitch = ({ currentColor }) => {
+  const { scene, nodes, materials } = useGLTF("./models/stitch.gltf");
+
+  useLayoutEffect(() => {
+    Object.assign(materials.Material, {
+      roughness: 0,
+      metalness: 0.25,
+      emissive: new THREE.Color(0x000000),
+      color: currentColor,
+      envMapIntensity: 0.5,
+    });
+  }, [scene, nodes, materials, currentColor]);
+
+  return <primitive object={scene} />;
+};
